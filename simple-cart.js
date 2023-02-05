291de78e166d27eb81770a47c328b76a7ddc9963
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-05 19:01:56 +0600

    add

--- simple-cart.js
+++ simple-cart.js
@@ -2,7 +2,7 @@ const shoppingCart = [
 { name: 'shoe', price: 1200 },
 { name: 'shirt', price: 2200 },
 { name: 'pant', price: 3700 },
-{ name: 'brlt', price: 700 },
+{ name: 'belt', price: 700 },
 ];
 
 function totalCost(products){
